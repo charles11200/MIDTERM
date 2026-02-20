58bd7e569b6b709526821b757a6aebbf29742901
charl <sisondesmer837@gmail.com>
2026-02-21 04:27:22 +0800

    "new"

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ Future<void> main() async {
 
   // ✅ Open boxes (order matters)
   await Hive.openBox('database');          // for balance, biometrics, saved_address
-  await Hive.openBox<Task>('tasks');       // typed box for Task asasdsadasd
+  await Hive.openBox<Task>('tasks');       // typed box for Task asasdsadasdcsc
 
   // ✅ Load saved address from Hive
   await AddressStore.instance.init();
